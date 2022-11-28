d41b2c013e9dd6db1258d83a1e51ab6abd2a47f7
Eduardo de Souza Ilha <eduardoilhasouza@gmail.com>
2022-11-28 00:47:02 -0300

    My framework

--- components/home/home.js
+++ components/home/home.js
@@ -1,12 +1,13 @@
-
 $('span:last').click(()=>{
     console.log("The best game for you")
    
-    $.get("ajax.html", function(res){
+    $.get("components/home/about.html", function(res){
         $('#container').html(res).css("display","flex").css("flex-direction","column");
     })
 })
-
 $('span:first').click(()=>{
- $('p').hide(5000).show(5000).fadeOut(5000).css("color","red")
+       $('#menu').remove()
+       $.get("components/game/game.html", function(res){
+        $('#game').html(res) 
+        })
 })
---
+++ index.js
@@ -0,0 +1,11 @@
+$.get("components/home/home.html", function(res){
+    $('#menu').html(res);
+})
+
+
+
+/*
+$('span:first').click(()=>{
+ $('p').hide(5000).show(5000).fadeOut(5000).css("color","red")
+})
+*/
