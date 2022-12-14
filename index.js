c090055a09c08258b4749d4115f2433ba50c1c21
Eduardo de Souza Ilha <02150179@aluno.canoas.ifrs.edu.br>
2022-12-14 16:30:02 -0300

    game 2.7

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 $.get("components/home/home.html", function(res){
     $('#menu').html(res);
+    home()  
     
 })
 
 let URL = document.createElement('base')
 URL.href= document.location.href       
 document.head.appendChild(URL)
+
+function home(){
+    $('span:last').click(()=>{
+        console.log("The best game for you", $("script"))
+       
+        $.get("components/home/about.html", function(res){
+            $('#container').html(res).css("display","flex").css("flex-direction","column");
+        })
+    
+    })
+    $('span:first').click(()=>{
+           $('#menu').children().remove()
+           $.get("components/game/game.html", function(res){
+            $('#game').html(res)
+            $('body').append() 
+            })
+    })
+}
+
+
+
+
